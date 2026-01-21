494f0f480fa51f3f99d7cea711ed77a927db39a8
maunzCache <166729+maunzCache@users.noreply.github.com>
2026-01-21 08:45:34 +0100

    Initial commit

---
+++ anger.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Source it and have fu... anger management
+
+alias goddamn="sudo "
+alias git-punch="git push --force"
+alias tableflip="goddamn shutdown now"
+
+# https://github.com/nvbn/thefuck
+alias get-fucked="pip install thefuck"
