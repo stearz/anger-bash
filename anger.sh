dde722369d0d6fffe253acde76c2a46b03a8dd7b
Stephan Schwarz <stephan.schwarz@iits-consulting.de>
2026-02-28 18:13:04 +0100

    add: please and breathe

--- anger.sh
+++ anger.sh
@@ -5,6 +5,22 @@
 alias goddamn="sudo "
 alias git-punch="git push --force"
 alias tableflip="goddamn shutdown now"
+alias please="sudo !!"
 
 # https://github.com/nvbn/thefuck
 alias get-fucked="pip install thefuck"
+
+# 1 minute breathing exercise:
+breathe() {
+	for i in {1..4}; do
+		printf "\nBreathe in"
+		for j in {1..4}; do printf "."; sleep 1;  done
+		printf " hold"
+		for j in {1..4}; do printf "."; sleep 1;  done
+		printf " breathe out"
+		for j in {1..4}; do printf "."; sleep 1;  done
+		printf " hold"
+		for j in {1..4}; do printf "."; sleep 1;  done
+	done
+  echo "\n\nGood, now breathe normally and hopefully you feel more relaxed."
+}
